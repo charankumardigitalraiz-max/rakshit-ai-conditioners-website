9323d245d6a2eb9e552d0aa4d70fa72e256876af
charankumardigitalraiz-max <charankumardigitalraiz@gmail.com>
2026-04-20 19:54:06 +0530

    whatsapp numbe rupdated

--- src/components/ui/Whatsapp-floating-button.jsx
+++ src/components/ui/Whatsapp-floating-button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Whatsappfloatingbutton = () => {
     return (
         <a
-            href="https://wa.me/919848629208"
+            href="https://wa.me/919030064466"
             target="_blank"
             rel="noopener noreferrer"
             className="fixed bottom-6 right-6 z-50 flex items-center justify-center w-14 h-14 bg-[#25D366] text-white rounded-full shadow-lg hover:shadow-2xl hover:-translate-y-1 transition-all duration-300 group"
